964d9f5637442488ac52d6c1ce53b9ce6bccea46
Fo-Sho <davidolivingstone@gmail.com>
2025-11-22 19:48:29 +0100

    Fix API routes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import api_overview, ProjectList, ProjectDetail
 
 urlpatterns = [
-    path('api/', api_overview, name='api-overview'),
-    path('api/projects/', ProjectList.as_view(), name='project-list'),
-    path('api/projects/<int:pk>/', ProjectDetail.as_view(), name='project-detail'),
+    path('', api_overview, name='api-overview'),
+    path('projects/', ProjectList.as_view(), name='project-list'),
+    path('projects/<int:pk>/', ProjectDetail.as_view(), name='project-detail'),
 ]
+
 
 

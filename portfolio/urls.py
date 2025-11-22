0d75840b39e68a078267eb5b269f5b83676f5eda
Fo-Sho <davidolivingstone@gmail.com>
2025-11-22 19:41:13 +0100

    Restore /api/projects/ endpoint

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import api_overview, ProjectList, ProjectDetail
 
 urlpatterns = [
-    path('', api_overview, name='api-overview'),
-    path('projects/', ProjectList.as_view(), name='project-list'),
-    path('projects/<int:pk>/', ProjectDetail.as_view(), name='project-detail'),
+    path('api/', api_overview, name='api-overview'),
+    path('api/projects/', ProjectList.as_view(), name='project-list'),
+    path('api/projects/<int:pk>/', ProjectDetail.as_view(), name='project-detail'),
 ]
+
 

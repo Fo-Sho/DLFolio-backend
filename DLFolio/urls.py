9b1a306a046e394e664992e33b3ad8c04fa48599
Fo-Sho <davidolivingstone@gmail.com>
2025-11-22 20:54:32 +0100

    Serve media files in production on Render

--- DLFolio/urls.py
+++ DLFolio/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('api/', include('portfolio.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in both development and production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
